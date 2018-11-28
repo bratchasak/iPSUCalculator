23f613c78c514aab77d02bed8ab3d5750b92117e
Wendy <wendy.wizardary@gmail.com>
2018-11-28 10:11:07 +0700

    Com

--- App.js
+++ App.js
@@ -7,23 +7,17 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import Calculator from './src/Calculator';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Calculator />
       </View>
     );
   }
@@ -32,18 +26,10 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+		backgroundColor: '#ff7675',
+		paddingLeft: 15,
+		paddingRight: 15,
+		alignItems: 'center',
+		justifyContent: 'center'
+  }
+})
