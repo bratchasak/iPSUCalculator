7a2b2e5165bbde69cf4ff3b9e3521220901c17be
Wendy <wendy.wizardary@gmail.com>
2018-11-29 16:20:28 +0700

    Commit

--- src/Calculator.js
+++ src/Calculator.js
@@ -40,7 +40,7 @@ export default class Calculator extends React.Component {
 		}
 	}
 
-	_handlePressOperator(operator) {	
+	_handlePressOperator(operator) {
 		const { variable2 } = this.state;
 		if (variable2) {
 			this.calculate();
@@ -87,78 +87,79 @@ export default class Calculator extends React.Component {
 			textResult = variable2;
 		} else {
 			textResult = variable1;
-		}
+		}	
 
-		return (			
+		return (
 			<View style={styles.body}>
-				<View style={styles.panelLabel}>
-					<Text style={styles.labelResult}>{_.round(textResult, 2).toString()}</Text>
+				<View style={styles.row}>
+					<View style={styles.col}>
+						<Text style={styles.labelResult}>{_.round(textResult, 2).toString()}</Text>
+					</View>
 				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>
+				<View style={styles.row}>
+					<View style={styles.col}>						
 						<ButtonInput text="1" onPress={() => this._handlePressNumber(1)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="2" onPress={() => this._handlePressNumber(2)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="3" onPress={() => this._handlePressNumber(3)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="4" onPress={() => this._handlePressNumber(4)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="5" onPress={() => this._handlePressNumber(5)} />
 					</View>
-				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>
+				</View>	
+				<View style={styles.row}>
+					<View style={styles.col}>
 						<ButtonInput text="6" onPress={() => this._handlePressNumber(6)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="7" onPress={() => this._handlePressNumber(7)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="8" onPress={() => this._handlePressNumber(8)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="9" onPress={() => this._handlePressNumber(9)} />
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<ButtonInput text="0" onPress={() => this._handlePressNumber(0)} />
 					</View>
-				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>
-						<ButtonInput text="+" onPress={() => this._handlePressOperator("+")} />
+				</View>	
+				<View style={styles.row}>
+					<View style={styles.col}>						
+						<ButtonInput text="+" onPress={() => this._handlePressOperator('+')} />
 					</View>
-					<View style={styles.colButton}>
-						<ButtonInput text="-" onPress={() => this._handlePressOperator("-")} />
+					<View style={styles.col}>
+						<ButtonInput text="-" onPress={() => this._handlePressOperator('-')} />
 					</View>
-					<View style={styles.colButton}>
-						<ButtonInput text="x" onPress={() => this._handlePressOperator("*")} />
+					<View style={styles.col}>
+						<ButtonInput text="x" onPress={() => this._handlePressOperator('*')} />
 					</View>
-					<View style={styles.colButton}>
-						<ButtonInput text="/" onPress={() => this._handlePressOperator("/")} />
+					<View style={styles.col}>
+						<ButtonInput text="/" onPress={() => this._handlePressOperator('/')} />
 					</View>
-				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>						
-						<ButtonInput text="AC" onPress={() => this._handlePressClear()} backgroundColor="#f39c12" textColor="#ffffff" />
+				</View>	
+				<View style={styles.row}>
+					<View style={styles.col}>
+						<ButtonInput text="AC" backgroundColor="#f39c12" textColor="#FFFFFF" onPress={() => this._handlePressClear()} />
 					</View>
-					<View style={styles.colButton}>						
+					<View style={styles.col}>
 						<ButtonInput text="=" onPress={() => this._handlePressResult()} />
 					</View>
-				</View>
-			</View>			
+				</View>				
+			</View>
 		)
 	}
 }
 
 const styles = StyleSheet.create({	
 	body: {
-		backgroundColor: '#ecf0f1',
-		height: 280,
+		backgroundColor: '#dfe6e9',
 		width: '100%',
 		paddingTop: 15,
 		paddingBottom: 15,
@@ -173,15 +174,11 @@ const styles = StyleSheet.create({
 		padding: 7,
 		textAlign: 'right'
 	},
-	rowButton: {
-		flex: 1,
-		flexDirection: 'row',
-		height: '33%',
-		paddingTop: 10,
-		paddingBottom: 10,		
+	row: {
+		flexDirection: 'row'
 	},
-	colButton: {
+	col: {
 		flex: 1,
 		padding: 5
-	}	
+	}
 })
