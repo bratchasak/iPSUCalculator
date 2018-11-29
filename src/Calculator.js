629b7b8882d0bed2b834e0cbd669a2a54b781841
Wendy <wendy.wizardary@gmail.com>
2018-11-29 16:04:13 +0700

    Commit

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,116 +2,120 @@ import React from 'react';
 import {
 	StyleSheet,
 	View,
-	TouchableHighlight,
-	Text
+	Text,
+	TouchableHighlight
 } from 'react-native';
 
 export default class Calculator extends React.Component {
-
 	render() {
-
-		return (			
+		return (
 			<View style={styles.body}>
-				<View style={styles.panelLabel}>
-					<Text style={styles.labelResult}></Text>
+				<View style={styles.row}>
+					<View style={styles.col}>
+						<Text style={styles.labelResult}>0</Text>
+					</View>
 				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>
+				<View style={styles.row}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>1</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>2</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>3</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>4</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>5</Text>
 						</TouchableHighlight>
 					</View>
-				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>
+				</View>	
+				<View style={styles.row}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>6</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>7</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>8</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>9</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>0</Text>
 						</TouchableHighlight>
 					</View>
-				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>
+				</View>	
+				<View style={styles.row}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>+</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>-</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>x</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>/</Text>
 						</TouchableHighlight>
 					</View>
-				</View>
-				<View style={styles.rowButton}>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
+						<TouchableHighlight style={styles.buttonInput}>
+							<Text style={styles.buttonText}>5</Text>
+						</TouchableHighlight>
+					</View>
+				</View>	
+				<View style={styles.row}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>AC</Text>
 						</TouchableHighlight>
 					</View>
-					<View style={styles.colButton}>
+					<View style={styles.col}>
 						<TouchableHighlight style={styles.buttonInput}>
 							<Text style={styles.buttonText}>=</Text>
 						</TouchableHighlight>
 					</View>
-				</View>
-			</View>			
+				</View>				
+			</View>
 		)
 	}
 }
 
-const styles = StyleSheet.create({	
+const styles = StyleSheet.create({
 	body: {
-		backgroundColor: '#ecf0f1',
-		height: 280,
+		backgroundColor: '#dfe6e9',
 		width: '100%',
 		paddingTop: 15,
 		paddingBottom: 15,
@@ -126,14 +130,10 @@ const styles = StyleSheet.create({
 		padding: 7,
 		textAlign: 'right'
 	},
-	rowButton: {
-		flex: 1,
-		flexDirection: 'row',
-		height: '33%',
-		paddingTop: 10,
-		paddingBottom: 10,		
+	row: {
+		flexDirection: 'row'
 	},
-	colButton: {
+	col: {
 		flex: 1,
 		padding: 5
 	},
