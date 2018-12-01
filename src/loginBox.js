21a013cefaec8a4f872b90cd8ad4a579e89e6969
Wendy <wendy.wizardary@gmail.com>
2018-12-02 00:00:59 +0700

    Commit

---
+++ src/loginBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+
+export default class LoginBox extends React.Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
